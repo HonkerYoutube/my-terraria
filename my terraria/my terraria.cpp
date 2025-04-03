7faf36e0e5faae51f81af88d79437e48538d41ee
hikee <honzikpaulik2@gmail.com>
2025-04-03 17:25:27 +0200

    Add project files.

---
+++ my terraria/my terraria.cpp
@@ -0,0 +1,12 @@
+﻿// my terraria.cpp : Defines the entry point for the application.
+//
+
+#include "my terraria.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
